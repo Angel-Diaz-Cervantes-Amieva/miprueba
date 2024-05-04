d13a7ce7bd7e88ab6a4c1e7adf810624f542870e
Angel Diaz Cervantes Amieva <angelitodc98@gmail.com>
2024-05-04 13:24:21 -0700

    Archivos de suma y resta creados existosamente.

---
+++ resta.java
@@ -0,0 +1,11 @@
+public class resta {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = a - b;
+        int d = 100;
+        int e = d - c;
+
+        System.out.println("d - c = " + e);
+    }
+}

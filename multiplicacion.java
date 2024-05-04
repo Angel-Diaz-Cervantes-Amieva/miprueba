9e8b7f17ef20e7a8e00170640dce0049112ab82a
Angel Diaz Cervantes Amieva <angelitodc98@gmail.com>
2024-05-04 13:35:23 -0700

    Archivo para multiplicar creado exitosamente.

---
+++ multiplicacion.java
@@ -0,0 +1,9 @@
+public class multiplicacion {
+    public static void main (String[] args) throws Exception{
+        int a = 3;
+        int b = 5;
+         int c = a * b;
+        
+        System.out.println("a * b = " + c);
+    }
+}

10c99a42321b41bb8868cc2ad4786b09dedb0672
Angel Diaz Cervantes Amieva <angelitodc98@gmail.com>
2024-05-04 13:36:15 -0700

    Archivo de multiplicacion eliminado.

--- multiplicacion.java
+++
@@ -1,9 +0,0 @@
-public class multiplicacion {
-    public static void main (String[] args) throws Exception{
-        int a = 3;
-        int b = 5;
-         int c = a * b;
-        
-        System.out.println("a * b = " + c);
-    }
-}

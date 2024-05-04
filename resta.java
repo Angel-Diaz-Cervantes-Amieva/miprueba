20a51736ec8a175f6cf69dafb839e2cf7d2b5725
Angel Diaz Cervantes Amieva <angelitodc98@gmail.com>
2024-05-04 13:28:15 -0700

    Método de resta creado y funcionando.

--- resta.java
+++ resta.java
@@ -2,10 +2,11 @@ public class resta {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        int c = a - b;
-        int d = 100;
-        int e = d - c;
+        Resta(a, b);
+    }
 
-        System.out.println("d - c = " + e);
+    public static void Resta(int a, int b) {
+        int c = a - b;
+        System.out.println("a - b = " + c);
     }
 }
